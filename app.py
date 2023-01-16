6c89ddcef79125ae438f95f8969c87fd715d5f2c
Tejas9740 <114503154+Tejas9740@users.noreply.github.com>
2023-01-16 20:34:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route("/")
+def main():
+    return "Welcome!"
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__== "_main_":
+    app.run()
